1094fcc522ede2c86c2c77903510493cfe3828da
Dong Yeop-Lee <33149488+hjyj4841@users.noreply.github.com>
2024-07-04 19:38:48 +0900

    main_텍스트 생성 애니메이션 완성 & 텍스트 이동 애니메이션 구현 중

--- script/mainScript.js
+++ script/mainScript.js
@@ -1,12 +1,16 @@
 const mHtml = document.querySelector("html");
 const ls = document.querySelectorAll("nav li > a");
-const circles = document.querySelectorAll("nav li > div");
+const circles = document.querySelectorAll(".circle");
+const circleRings = document.querySelectorAll(".circle + div");
 const sections = document.querySelectorAll("main > section");
+const maintexts = document.querySelectorAll("#con1 div > h1 ");
 
-var page = 0; // 현재 표시되는 페이지
-var lastPage = sections.length - 1; // 마지막 페이지(현재는 mian > div)
+let page = 0; // 현재 표시되는 페이지
+const lastPage = sections.length - 1; // 마지막 페이지(현재는 mian > div)
+const testString = "HELLO, WORLD!";
+let homeTimer = 0;
 
-// 현재 위치한 윈도우창의 상단 부분 이동하는 html
+// 현재 위치한 윈도우창의 상단 부분으로 이동
 function scrollTop(el, value) {
   var win;
   if (el.window === el) {
@@ -20,10 +24,32 @@ function scrollTop(el, value) {
   }
 
   if (win) {
-    win.scrollTo(win.pageXOffset, value);
+    win.scrollTop(win.pageXOffset, value);
   } else {
     el.scrollTop = value;
   }
+}
+
+// Home에 텍스트 이벤트 들어가는 함수
+function mainTextAnimation(count) {
+  setTimeout(() => {
+    maintexts[count].textContent += testString[homeTimer];
+    homeTimer++;
+
+    if (homeTimer < testString.length) mainTextAnimation(count);
+    else {
+      maintexts[count].style.animation = "none";
+      // 2번 3번 애니메이션 이어지도록
+      homeTimer = 0;
+      count++;
+      if (count < 3) {
+        maintexts[count].style.animation = "fakeKey 0.8s step-end infinite";
+        mainTextAnimation(count);
+      } //else {   // 이어서 나올 위치 이동 애니메이션 구현 중
+      //   maintexts[0].parentNode.style.animation = "move1 1s linear forwards";
+      // }
+    }
+  }, 200);
 }
 
 // scroll 못하게 막는 event
@@ -37,8 +63,6 @@ window.addEventListener(
 
 // wheel을 돌렸을 때 발생하는 event
 window.addEventListener("wheel", function (e) {
-  // if (mHtml.is(":animated")) return;
-
   if (e.deltaY > 0) {
     if (page == 4) return;
     page++;
@@ -57,13 +81,11 @@ window.addEventListener("scroll", function (e) {
     if (sections[i].getBoundingClientRect().top == 0) {
       ls[i].style.color = "rgb(74, 171, 250)";
       circles[i].style.backgroundColor = "rgb(74, 171, 250)";
-      circles[i].style.width = "8px";
-      circles[i].style.height = "8px";
+      circleRings[i].classList.add("ring");
     } else {
       ls[i].style.color = "gray";
       circles[i].style.backgroundColor = "gray";
-      circles[i].style.width = "5px";
-      circles[i].style.height = "5px";
+      circleRings[i].classList.remove("ring");
     }
   }
 });
@@ -75,7 +97,7 @@ for (let i = 0; i <= lastPage; i++) {
   });
 }
 
-// 새로고침 시 상단으로 강제 이동
-window.addEventListener("load", function () {
-  scrollTop(mHtml, 0);
-});
+// 페이지 로드시 첫화면 애니메이션 발생
+mainTextAnimation(0);
+// 페이지 로드시 html 상단으로 이동
+scrollTop(mHtml, 0);
