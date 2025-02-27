9930889b142046e90f1dbfb34d3e11e52b2db2e6
Dong Yeop-Lee <33149488+hjyj4841@users.noreply.github.com>
2025-02-28 05:33:27 +0900

    레이아웃 전체적으로 수정, 내용 수정

--- script/mainScript.js
+++ script/mainScript.js
@@ -1,35 +1,80 @@
-const mHtml = document.querySelector("html");
-const nav = document.querySelector("nav");
-const ls = document.querySelectorAll("nav li > a");
-const circles = document.querySelectorAll(".circle");
-const circleRings = document.querySelectorAll(".circle + div");
-const sections = document.querySelectorAll("main > section");
-const maintexts = document.querySelectorAll("#home div > h1 ");
-const infoSelects = document.querySelectorAll("#informationSelect > div");
-const infoTexts = document.querySelectorAll("#informationText > div");
-
-const mainStr = ["WEB DEVELOPER", "LEE DONG-YEOP", "PORTPOLIO"];
-let homeTimer = 0;
-
-// Home에 텍스트 이벤트 들어가는 함수
-function mainTextAnimation(count) {
-  setTimeout(() => {
-    maintexts[count].textContent += mainStr[count][homeTimer];
-    homeTimer++;
-
-    if (homeTimer < mainStr[count].length) mainTextAnimation(count);
-    else {
-      maintexts[count].style.animation = "none";
-      // 2번 3번 애니메이션 이어지도록
-      homeTimer = 0;
-      count++;
-      if (count < 3) {
-        maintexts[count].style.animation = "fakeKey 0.5s step-end infinite";
-        mainTextAnimation(count);
+// 메인 이벤트
+function mainTextAnimation() {
+  $("#home-section div").each((i, section) => {
+    setTimeout(() => {
+      $(section).css({
+        textShadow: "3px 3px 30px #555555",
+        color: "white"
+      });
+      
+      if (i < $("#home-section div").length - 1) {
+        $($(".main-text")[i]).css({
+          color: "#555555",
+          transition: "3s"
+        });
+      } else {
+        $(".main-name").css({
+          color: "#555555",
+          transition: "3s"
+        });
       }
-    }
-  }, 100);
+    }, i === 0 ? 100 : (i * 1000));
+  });
 }
 
 // 페이지 로드시 첫화면 애니메이션 발생
-mainTextAnimation(0);
+mainTextAnimation();
+
+// 스크롤 이벤트
+$(window).scroll(function(){
+    
+  const about = $("#about-section").offset().top;
+  let height = $(document).scrollTop();
+  const windowHeight = $(window).height(); // 윈도우 높이
+
+    if(about <= height){
+      $("header").css("backgroundColor", "rgba(205, 201, 195, 0.8)");
+    } else {
+      $("header").css("backgroundColor", "rgba(85, 85, 85, 0.8)");
+    }
+
+    $(".sc-event").each(function() {
+      const sc = $(this);
+
+      if (height + windowHeight * 0.8 > sc.offset().top) {
+        sc.css({
+          animation: "fadeInUp 1.5s",
+          opacity: "1"});
+      } 
+    });
+});
+
+// 포트폴리오 이미지 변경
+const flowImages = $("#flow-img img");
+let flowIndex = 0;
+const vibeImages = $("#vibe-img img");
+let vibeIndex = 0;
+
+$("#flow-prev").click(function() {
+  $(flowImages[flowIndex]).animate({ opacity: 0 }, 500);
+  flowIndex = (flowIndex - 1 + flowImages.length) % flowImages.length;
+  $(flowImages[flowIndex]).animate({ opacity: 1 }, 500);
+});
+
+$("#flow-next").click(function() {
+  $(flowImages[flowIndex]).animate({ opacity: 0 }, 500);
+  flowIndex = (flowIndex + 1) % flowImages.length;
+  $(flowImages[flowIndex]).animate({ opacity: 1 }, 500);
+});
+
+$("#vibe-prev").click(function() {
+  $(vibeImages[vibeIndex]).animate({ opacity: 0 }, 500);
+  vibeIndex = (vibeIndex - 1 + vibeImages.length) % flowImages.length;
+  $(vibeImages[vibeIndex]).animate({ opacity: 1 }, 500);
+});
+
+$("#vibe-next").click(function() {
+  $(vibeImages[vibeIndex]).animate({ opacity: 0 }, 500);
+  vibeIndex = (vibeIndex + 1) % vibeImages.length;
+  $(vibeImages[vibeIndex]).animate({ opacity: 1 }, 500);
+});
